77f68f6b5c3a5fc1371348caccb7e50da464e413
Sakaisha <aishasakkary@outlook.com>
2024-10-16 17:36:33 -0700

    all

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
-import { Router } from 'express';
-import appRouter from './app';
-import authRouter from './auth';
-import usersRouter from './users';
-import filesRouter from './files';
+const express = require('express');
 
-const router = Router();
-router.use(appRouter);
-router.use(authRouter);
-router.use(usersRouter);
-router.use(filesRouter);
+const router = express.Router();
 
-export default router;
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
+
+module.exports = router;
